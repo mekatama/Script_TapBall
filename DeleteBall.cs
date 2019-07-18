28b9f80f48c273e32522edfc7c97e746bc17e8a1
mekatama <tamatte@hotmail.com>
2019-07-18 19:57:54 +0900

    ball 一定時間でdel処理

---
+++ DeleteBall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteBall : MonoBehaviour {
+//	private float tempScale = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject,0.15f);	//オブジェクトの削除
+	}
+
+	void Update () {
+//		tempScale -= 0.1f;
+//		this.transform.localScale = new Vector3(tempScale, tempScale, tempScale);
+	}
+}
