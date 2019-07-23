cc42956fe1fbdef67775e7cf1fc20e53fd90af2e
mekatama <tamatte@hotmail.com>
2019-07-23 19:25:36 +0900

    スコアUI表示

---
+++ UI_TotalScore.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_TotalScore : MonoBehaviour{
+	public GameObject gameController;	//GameController取得
+	public Text totalScoreText;			//Textコンポーネント取得用
+
+    void Update(){
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		//最大tap数表示
+	    totalScoreText.text = "score : " + gc.totalScore.ToString("000000");
+    }
+}
