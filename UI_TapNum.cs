e6460e0f3de8fadf7078476b4b693a0e23b4004f
mekatama <tamatte@hotmail.com>
2019-07-23 19:25:16 +0900

    残りタッチ数UI

---
+++ UI_TapNum.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_TapNum : MonoBehaviour{
+	public GameObject gameController;	//GameController取得
+	public Text tapNumText;				//Textコンポーネント取得用
+
+    void Update(){
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		//最大tap数表示
+	    tapNumText.text = "nokori tap num : " + gc.touchNumMax.ToString("000");
+    }
+}
