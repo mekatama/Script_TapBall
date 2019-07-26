e6fdb3c076cd745a9c84c96c1d6b2a39bca5f2fc
mekatama <tamatte@hotmail.com>
2019-07-26 19:57:49 +0900

    outgame用のUI制御

---
+++ OutGameController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OutGameController : MonoBehaviour{
+	void Update(){
+		//backキー
+		if (Input.GetKeyUp(KeyCode.Escape)){
+			Application.Quit();	//アプリ終了
+		}
+	}
+
+	//main用の制御関数
+	public void ButtonClicked_Start1(){
+		SceneManager.LoadScene("main1");	//シーンのロード
+	}
+
+	//遊び方ボタン用の制御関数
+	public void ButtonClicked_HowToPlay(){
+		SceneManager.LoadScene("howtoplay");	//シーンのロード
+	}
+
+	//return用の制御関数
+	public void ButtonClicked_Return(){
+		SceneManager.LoadScene("title");	//シーンのロード
+	}
+
+	//アプリ終了
+	public void ButtonClicked_Exit(){
+		Application.Quit();
+		Debug.Log("exit");	
+	}
+/*
+	//Debug用ハイスコアリセットボタン
+	public void ButtonClicked_Reset(){
+		PlayerPrefs.DeleteAll();
+		Debug.Log("全データ削除しますた");	
+	}
+*/
+}
