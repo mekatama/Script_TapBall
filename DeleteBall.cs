e83a95550db93d0add0f0b6c0f82277d64d06025
mekatama <tamatte@hotmail.com>
2019-07-31 20:36:08 +0900

    targetと接触で消す処理

--- DeleteBall.cs
+++ DeleteBall.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class DeleteBall : MonoBehaviour {
 //	private float tempScale = 1.0f;
 
-	// Use this for initialization
 	void Start () {
 		Destroy(gameObject,0.15f);	//オブジェクトの削除
 	}
@@ -13,5 +12,13 @@ public class DeleteBall : MonoBehaviour {
 	void Update () {
 //		tempScale -= 0.1f;
 //		this.transform.localScale = new Vector3(tempScale, tempScale, tempScale);
+	}
+
+	//他のオブジェクトとの当たり判定
+	void OnCollisionEnter(Collision other) {
+		if(other.gameObject.tag == "TargetBall"){
+			Debug.Log("hit");
+			Destroy(gameObject);	//このGameObjectを［Hierrchy］ビューから削除する
+		}
 	}
 }
