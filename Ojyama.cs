872f3a705b0b7c0c79d43df9292db4150e030ee7
mekatama <tamatte@hotmail.com>
2019-08-03 15:27:08 +0900

    障害物お回転挙動入れる

---
+++ Ojyama.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ojyama : MonoBehaviour
+{
+    public bool isRotation; //回転flag
+    public int rot_x;       //回転値
+    public int rot_y;       //
+public int rot_z;       //
+
+    void Update(){
+        if(isRotation){
+    		transform.Rotate(new Vector3(rot_x, rot_y, rot_z) * Time.deltaTime);
+        }
+    }
+}
